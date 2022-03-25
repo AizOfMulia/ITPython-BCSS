fd8a25937f228b946921df32bd414e5446cc2317
AizOfMulia <aizofmulia@gmail.com>
2022-03-25 15:58:21 +0800

    Added test data library
    Signed-off-by: AizOfMulia <aizofmulia@gmail.com>

---
+++ test_data/account.py
@@ -0,0 +1,14 @@
+class account:
+    users : dict    =   {
+                            "shazam" :
+                            {
+                                "password" : "185f8db32271fe25f561a6fc938b2e264306ec304eda518007d1764826381969",
+                                "name" : "Adam"
+                            },
+
+                            "batman" :
+                            {
+                                "password" : "a982c97d452cc71fde02409d6e6a623220c882930c3c13ab7733e61bc9870b6c",
+                                "name" : "Bruce"
+                            },
+                        }
